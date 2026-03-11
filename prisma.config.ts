a1b1a7eeab81028fa702b8b5e5ab63f076bef04b
Sarem Ketema <saremketema12@gmail.com>
2026-03-11 22:56:09 +0300

    feat: add initial Prisma configuration and schema files

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config"; // <--- This MUST be the first line
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
